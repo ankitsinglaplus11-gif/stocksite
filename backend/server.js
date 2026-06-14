ccabf28ce03c0b0be0c559cedf9c01915f05a5ed
Ankit <your-email@example.com>
2026-06-14 19:54:38 +0530

    Render deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1855,12 +1855,11 @@ app.get(
 );
 /* ---------------- SERVER ---------------- */
 
-const PORT = 5000;
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
   console.log(
     `Server running on port ${PORT}`
   );
-
 });
